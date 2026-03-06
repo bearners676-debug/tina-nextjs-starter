e376fe9fb644317c097875369ca70864ed958cf8
Bumb Lee <b.earners676@gmail.com>
2026-03-06 23:50:12 +0530

    Add homepage

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function HomePage() {
+  return (
+    <div style={{ padding: '4rem 5%', textAlign: 'center' }}>
+      <h1 style={{ fontSize: '3rem', marginBottom: '1rem', color: '#7CB342' }}>
+        B-<span style={{ color: '#222' }}>earners</span>
+      </h1>
+      <p style={{ fontSize: '1.2rem', color: '#666' }}>
+        Digital Products Store - Coming Soon!
+      </p>
+    </div>
+  );
+}
