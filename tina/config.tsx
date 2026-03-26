27c2a219a0df8dc26b26f9858faf793655a6c7c7
Bumb Lee <b.earners676@gmail.com>
2026-03-26 11:29:12 +0530

    Update config.tsx

--- tina/config.tsx
+++ tina/config.tsx
@@ -1,6 +1,6 @@
 import { defineConfig } from "tinacms";
 
-const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+const branch = "main";
 
 export default defineConfig({
   branch,
